ac2c87bf054068d7bcf1e2e5cd8cc82251a995d5
tanvirch0750 <tanvirch7575@gmail.com>
2024-01-08 00:04:22 +0600

    setup project and basic folder structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+function AuthLayout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
+
+export default AuthLayout;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <p className="text-3xl font-bold text-emerald-500">
+      This is a protected route
+    </p>
+  );
+}
