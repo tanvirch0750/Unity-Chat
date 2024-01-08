0484daae4c8673337d0a33b34646e9c9e9fecaf7
tanvirch0750 <tanvirch7575@gmail.com>
2024-01-08 13:06:10 +0600

    implemented authentication with clerk

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-function RegisterPage() {
-  return (
-    <div>
-      <h1>This is register page</h1>
-    </div>
-  );
-}
-
-export default RegisterPage;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+
 export default function Home() {
   return (
     <p className="text-3xl font-bold text-emerald-500">
       This is a protected route
+      <UserButton afterSignOutUrl="/sign-in" />
     </p>
   );
 }
