3bfa236c1c6dfe997ffd4fa673f9cb7a491cf8c0
tanvirch0750 <tanvirch7575@gmail.com>
2024-01-10 01:10:21 +0600

    upload image with uploadthing

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -23,6 +23,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect, useState } from 'react';
+import FileUpload from '@/components/file-upload';
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -76,7 +77,21 @@ function InitialModal() {
           <form onSubmit={form.handleSubmit(onSumbit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                TODO: Image upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
               </div>
               <FormField
                 control={form.control}
